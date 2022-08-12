92eb8b5a2ff6fd4c648ab4c2ffa57de674acb3a0
Petr Borzutzky <borzutzky@gmail.com>
2022-08-12 16:50:21 +0200

    Create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">PC komponenty</a>
+      </header>
+      <main>Seznam komponent</main>
+    </div>
+  );
+}
+
+export default App;
